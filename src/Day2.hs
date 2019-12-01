16d4c930dc59a065313a6811ab86c28276c52457
Carsten König <Carsten.Koenig@hotmail.de>
2019-12-01 09:00:20 +0100

    Prepared for Day 2

---
+++ src/Day2.hs
@@ -0,0 +1,23 @@
+module Day2 where
+
+import Data.List (unfoldr)
+
+type Input = String
+
+run :: IO ()
+run = do
+  putStrLn "DAY 2"
+
+  input <- loadInput
+
+  let part1 = 0
+  putStrLn $ "\t Part 1: " ++ show part1
+
+  let part2 = 0
+  putStrLn $ "\t Part 2: " ++ show part2
+
+  putStrLn "---\n"
+
+
+loadInput :: IO Input
+loadInput = readFile "./src/day2.txt"
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
 import qualified Day1 as D1
+import qualified Day2 as D2
 
 main :: IO ()
 main = do
   D1.run
+  D2.run
   putStrLn "done"
