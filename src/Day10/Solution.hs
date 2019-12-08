a7cea04f570d08e8c32f214fed09dbc2de3470c9
Carsten König <Carsten.Koenig@hotmail.de>
2019-12-08 10:36:13 +0100

    prepared day 9 & 10

---
+++ src/Day10/Solution.hs
@@ -0,0 +1,33 @@
+module Day10.Solution where
+
+import           CommonParsers
+import           ConsoleTests
+
+
+type Input = String
+
+run :: IO ()
+run = do
+  putStrLn "DAY 10"
+
+  inp <- loadInput
+
+  let res1 = part1 inp
+  putStrLn $ "\t Part 1: " ++ show res1
+
+  let res2 = part2 inp
+  putStrLn $ "\t Part 2: " ++ show res2
+
+  putStrLn "---\n"
+
+
+part1 :: Input -> Int
+part1 inp = undefined
+
+
+part2 :: Input -> Int
+part2 inp = undefined
+
+
+loadInput :: IO Input
+loadInput = readFile "./src/Day10/input.txt"
