a7fb20f732e980ae6ea18f556e9e2f8fb466b153
Carsten König <Carsten.Koenig@hotmail.de>
2019-12-13 14:59:49 +0100

    simplified Day13 with a pure variant of IntCode

--- src/Day13/Solution.hs
+++ src/Day13/Solution.hs
@@ -1,14 +1,11 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE BangPatterns #-}
 module Day13.Solution where
 
-import           Control.Concurrent (forkIO)
-import           Control.Concurrent.STM (STM, atomically, orElse)
-import qualified Control.Concurrent.STM.TQueue as TQ
-import qualified Control.Concurrent.STM.TMVar as TM
 import           Coords
 import qualified Data.Map.Strict as M
 import           Data.Map.Strict (Map)
-import           IntCodeStm
+import           IntCodePure
 
 
 dayNr :: Int
@@ -22,10 +19,10 @@ run = do
 
   prg <- loadInput
 
-  blocks <- part1 prg
+  let blocks = part1 prg
   putStrLn $ "\t Part 1: " ++ show blocks
 
-  score <- runGame prg
+  let score = runGame prg
   putStrLn $ "\t Part 2: " ++ show score
 
   putStrLn "---\n"
@@ -38,10 +35,11 @@ type Coin = Int
 
 -- | possible outputs of the games engine
 data Output
-  = Draw Coord TileId
-  | Score Score
-  | Halted
-  deriving Show
+  = Draw Coord TileId (Continuation Int)
+  | Score Score (Continuation Int)
+  | WaitForInput (Int -> Continuation Int)
+  | Stopped
+
 
 type Display = Map Coord TileId
 
@@ -68,32 +66,49 @@ fromJoystick :: Joystick -> Int
 fromJoystick = (\x -> x - 1) . fromEnum
 
 
--- | internal Controls for the game
-data Controls = Controls
-  { getOutput   :: STM Output
-  , setJoystick :: Joystick -> STM ()
-  }
-
 
 ----------------------------------------------------------------------
 -- part 1
 
-part1 :: Input -> IO Int
-part1 prg = do
-  controls <- initGame prg
-  tileIds <- collectAllTileOutput controls
-  pure $ length $ filter (== Block) tileIds
+part1 :: Input -> Int
+part1 prg =
+  let tileIds = getTiles $ initComputer prg
+  in length $ filter (== Block) tileIds
 
 
-collectAllTileOutput :: Controls -> IO [TileId]
-collectAllTileOutput Controls{..} = reverse <$> go []
-    where 
-    go acc = do
-      next <- atomically getOutput 
-      case next of
-        Halted -> pure acc
-        Score _ -> pure acc
-        Draw _ tileId -> go (tileId:acc)
+
+-- | runs the computer to Stopped or the first Score
+--   and collects all tiles outputed
+getTiles :: Continuation Int -> [TileId]
+getTiles = go []
+  where
+  go acc cont =
+    case getOutput cont of
+      Draw _ tileId cont' -> go (tileId:acc) cont'
+      _                   -> acc
+
+
+-- | translates the IntCode-computers output into the problems output
+getOutput :: Continuation Int -> Output
+getOutput cont =
+  case collect 3 [] cont of
+    -- output of -1/0/score indicates a score output
+    Right ([-1, 0, score], cont') -> Score score cont'
+    -- else it will be coordinates and a tile-id
+    Right ([x, y, tileId], cont') -> Draw (x,y) (toEnum tileId) cont'
+    Right _ -> error "not enough output"
+    -- non-outputs will be passed on
+    Left res -> res
+  where
+  -- | tries to read 3 consecutive outputs from the computer
+  collect :: Int -> [Int] -> Continuation Int -> Either Output ([Int], Continuation Int)
+  collect 0 !acc !c = Right (reverse acc, c)
+  collect n !acc !c =
+    case runComputer c of
+      NewOutput a c' -> collect (n-1) (a:acc) c'
+      RequestInput i2c -> Left (WaitForInput i2c)
+      Error s -> error s
+      Halted -> Left Stopped
 
 
 ----------------------------------------------------------------------
@@ -101,96 +116,40 @@ collectAllTileOutput Controls{..} = reverse <$> go []
 
 -- | runs the game
 --   uses the obvious strategy of always moving towards the ball
-runGame :: Program Int -> IO Score
-runGame prg = do
+runGame :: Input -> Score
+runGame prg =
   -- init the game and set the coin-"counter" to 2 ... want to play for free
-  controls <- initGame (2 : tail prg)
+  let cont = initComputer (2 : tail prg)
   -- start the loop
-  go controls M.empty 0 Nothing Nothing
+  in go cont M.empty 0 Nothing Nothing
   where
     -- | the main loop - keeps track of the display, score, the paddle and ball position
-    go :: Controls -> Display -> Score -> Maybe Coord -> Maybe Coord -> IO Int
+    go :: Continuation Int -> Display -> Score -> Maybe Coord -> Maybe Coord -> Score
     -- if both ball and paddle positions are known
-    go c d s (Just paddleP) (Just ballP) = do
-      -- move towards ball
-      moveJoystick c paddleP ballP
-      -- and reset the positions - so that the game will only receive input again
-      -- after the ball position got updated
-      -- need to do this because otherwise the input buffer will be flooded with
-      -- wrong input resulting in a loss
-      go c d s (Just paddleP) Nothing
-    go c@Controls{..} display score paddleP ballP = do
-      -- get the next output from the game
-      out <- atomically getOutput
-      case out of
-          Score newScore -> 
-            -- update the score and continue
-            go c display newScore paddleP ballP
-          Draw coord tileId -> do
-            -- update paddle or ball pos if redrawn
-            -- the game will only clear and redraw
-            -- stuff that changed!
-            let (paddleP', ballP') =
-                  case tileId of
-                    Ball -> (paddleP, Just coord)
-                    Paddle -> (Just coord, ballP)
-                    _ -> (paddleP, ballP)
-            -- continue with updated display, paddle and ball positions
-            go c (M.insert coord tileId display) score paddleP' ballP'
-          Halted -> 
-            -- if the game halted return the score
-            pure score
-
-
--- | move paddle towards ball
-moveJoystick :: Controls -> Coord -> Coord -> IO ()
-moveJoystick Controls{..} paddlePos ballPos = 
-  atomically $ setJoystick (decide paddlePos ballPos)
-  where
-    decide (pX, _) (bX,_) 
-      | pX > bX = JoyLeft
-      | pX < bX = JoyRight
-      | otherwise = JoyNeutral
+    go cont display score paddleP ballP =
+      case getOutput cont of
+        Stopped -> score
+        Score newScore cont' -> go cont' display newScore paddleP ballP
+        WaitForInput inp2cont -> go (inp2cont $ getJoystick paddleP ballP) display score paddleP ballP
+        Draw coord tileId cont' ->
+          -- update paddle or ball pos if redrawn
+          -- the game will only clear and redraw
+          -- stuff that changed!
+          let (paddleP', ballP') =
+                case tileId of
+                  Ball -> (paddleP, Just coord)
+                  Paddle -> (Just coord, ballP)
+                  _ -> (paddleP, ballP)
+          -- continue with updated display, paddle and ball positions
+          in go cont' (M.insert coord tileId display) score paddleP' ballP'
+    getJoystick (Just (pX, _)) (Just (bX, _))
+      | pX > bX = fromJoystick JoyLeft
+      | pX < bX = fromJoystick JoyRight
+    getJoystick _ _ = fromJoystick JoyNeutral
 
 
 ----------------------------------------------------------------------
 -- Game setup
-
--- | run the given input as a game of breakout
---   note that the input is a buffer (Queue) and this runs
---   in the background so we have to be careful with the timing and
---   frequency of calling 'setJoystick' - a working solution is to do it
---   only after ball redraws
-initGame :: Program Int -> IO Controls
-initGame prg = do
-  output <- TQ.newTQueueIO
-  joystick <- TQ.newTQueueIO
-  halt <- TM.newEmptyTMVarIO
-
-  let is_halt = TM.takeTMVar halt
-  let getInput = TQ.readTQueue joystick
-  let nextOutput = readOutput output `orElse` (Halted <$ is_halt)
-
-  let game = createComputer "Game" prg getInput (TQ.writeTQueue output)
-  _ <- forkIO $ do
-      res <- runComputer game
-      case res of
-        Left err -> print err
-        Right () -> pure ()
-      atomically $ TM.putTMVar halt ()
-
-  pure $ Controls nextOutput (set_joystick joystick)
-  where
-    set_joystick v = TQ.writeTQueue v . fromJoystick
-    readOutput q = do
-      x <- TQ.readTQueue q
-      y <- TQ.readTQueue q
-      if x == -1 && y == 0
-        then
-          Score <$> TQ.readTQueue q
-        else
-          Draw (x,y) . toEnum <$> TQ.readTQueue q
-
 
 loadInput :: IO Input
 loadInput = parseProgram <$> readFile ("./src/Day" ++ show dayNr ++ "/input.txt")
---
+++ src/IntCodePure.hs
@@ -0,0 +1,386 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE RankNTypes, ScopedTypeVariables #-}
+
+module IntCodePure
+  ( Program, Computer
+  , Address
+  , Result (..)
+  , Continuation
+  , initComputer
+  , runComputer
+  , parseProgram
+  , getMemory
+  , setMemory
+  ) where
+
+
+import qualified Control.Monad.Except as Except
+import           Control.Monad.Except (MonadError, Except)
+import qualified Control.Monad.State.Strict as State
+import           Control.Monad.State.Strict (StateT, MonadState)
+import qualified Data.Map.Strict as Map
+import           Data.Map.Strict (Map)
+
+
+type Address adr = adr
+type Memory n = Map n n
+type Program n = [n]
+
+
+data Computer n = Computer
+  { memory             :: Memory n
+  , instructionPointer :: Address n
+  , relativeBase       :: Address n
+  , nextInput          :: Maybe n
+  }
+
+
+data Result n
+  = Halted
+  | NewOutput n (Continuation n)
+  | RequestInput (n -> Continuation n)
+  | Error String
+
+
+newtype Continuation n = Continuation { getComputer :: Computer n }
+
+----------------------------------------------------------------------
+-- OpCode and Instructions
+
+-- | our computers command-set
+data Instruction n
+  = Halt
+  | Add (Parameter n) (Parameter n) (Parameter n)
+  | Mult (Parameter n) (Parameter n) (Parameter n)
+  | Input (Parameter n)
+  | Output (Parameter n)
+  | JumpIfTrue (Parameter n) (Parameter n)
+  | JumpIfFalse (Parameter n) (Parameter n)
+  | LessThan (Parameter n) (Parameter n) (Parameter n)
+  | Equals (Parameter n) (Parameter n) (Parameter n)
+  | AdjustRelativeBase (Parameter n)
+  deriving (Show)
+
+
+-- | a parameter for an instruction
+--   each parameter has a mode and a value
+--   the mode flags if the value is to be interpreted as an address or as an value
+data Parameter n = Parameter
+  { parameterMode  :: ParameterMode
+  , parameterValue :: n
+  } deriving (Show)
+
+
+-- | the position mode determins if a parameter is
+--   interpreted as a pointer to a location ('PositionMode')
+--   or as an actual constant value ('ImmediateMode')
+data ParameterMode
+  = PositionMode
+  | ImmediateMode
+  | RelativeMode
+  deriving (Show, Eq)
+
+
+-- | parses the parameter mode from the numbers given in the problem/description
+parameterModeFromInt :: (Num n, Eq n, Show n) => MonadError (Result n) m => n -> m ParameterMode
+parameterModeFromInt 0 = pure PositionMode
+parameterModeFromInt 1 = pure ImmediateMode
+parameterModeFromInt 2 = pure RelativeMode
+parameterModeFromInt m = Except.throwError $ Error $ "invalid parameter-mode " ++ show m
+
+
+-- | calculates the length of an instruction
+--   so that we can move the instruction-pointer to the next one
+instructionLength :: Num n => Instruction n -> n
+instructionLength Halt = 1
+instructionLength Add{} = 4
+instructionLength Mult{} = 4
+instructionLength Input{} = 2
+instructionLength Output{} = 2
+instructionLength JumpIfTrue{} = 3
+instructionLength JumpIfFalse{} = 3
+instructionLength LessThan{} = 4
+instructionLength Equals{} = 4
+instructionLength AdjustRelativeBase{} = 2
+
+
+----------------------------------------------------------------------
+-- Interpretation of Programms
+
+-- | using a stack of an state-monad containing the 'Program' and
+--   Except String for error messages
+--   over STM
+type IntCodeM n a = StateT (Computer n) (Except (Result n)) a
+
+
+initComputer :: (Eq n, Integral n, Show n) => Program n -> Continuation n
+initComputer = Continuation . createComputer
+
+
+runComputer :: (Eq n, Integral n, Show n) => Continuation n -> Result n
+runComputer = stepComputer . getComputer
+
+
+stepComputer :: (Eq n, Integral n, Show n) => Computer n -> Result n
+stepComputer = either id (const Halted) . Except.runExcept . State.evalStateT executeProgram
+
+
+createComputer :: (Integral n, Show n, Eq n) => Program n -> Computer n
+createComputer prg =
+  Computer (Map.fromList $ zip [0..] prg) 0 0 Nothing
+
+
+
+-- | keeps running instructions until halt
+executeProgram :: (Eq n, Integral n, Show n) => IntCodeM n ()
+executeProgram = do
+  instr <- getCurrentInstruction
+  case instr of
+    Halt -> pure ()
+    _ -> runInstruction instr >> executeProgram
+
+
+-- | runs a single instruction according to the specifications of Day2 cc
+runInstruction :: (Eq n, Integral n, Show n) => Instruction n -> IntCodeM n ()
+runInstruction Halt =
+  -- no-op on halt
+  pure ()
+runInstruction (Add locA locB tgt) = do
+  -- add the value in from the first two given parameters and
+  -- write the result to the third's address (only valid if it is in 'PositionMode')
+  summandA <- evalParameter locA
+  summandB <- evalParameter locB
+  locTgt <- evalAddress tgt
+  setMemory locTgt (summandA + summandB)
+  moveNext
+runInstruction (Mult locA locB tgt) = do
+  -- multiplies the value in from the first two given parameters and
+  -- write the result to the third's address (only valid if it is in 'PositionMode')
+  operandA <- evalParameter locA
+  operandB <- evalParameter locB
+  locTgt <- evalAddress tgt
+  setMemory locTgt (operandA * operandB)
+  moveNext
+runInstruction (JumpIfTrue cond loc) = do
+  -- jumps to the address of the second parameter if the value of the first is
+  -- non-zero - if not just moves to the next instruction
+  condVal <- evalParameter cond
+  jumpAdr <- evalParameter loc
+  if condVal == 0
+    then moveNext
+    else jumpTo (fromIntegral jumpAdr)
+runInstruction (JumpIfFalse cond loc) = do
+  -- jumps to the address of the second parameter if the value of the first is
+  -- zero - if not just moves to the next instruction
+  condVal <- evalParameter cond
+  jumpAdr <- evalParameter loc
+  if condVal == 0
+    then jumpTo (fromIntegral jumpAdr)
+    else moveNext
+runInstruction (LessThan a b tgt) = do
+  -- compares parameter-values of the first and second parameter
+  -- writes 1 to the address of the last parameter if the first value is less than the second
+  -- if not writes 0
+  locTgt <- evalAddress tgt
+  aVal <- evalParameter a
+  bVal <- evalParameter b
+  let res = if aVal < bVal then 1 else 0
+  setMemory locTgt res
+  moveNext
+runInstruction (Equals a b tgt) = do
+  -- compares parameter-values of the first and second parameter
+  -- writes 1 to the address of the last parameter if the first value is equal to the second
+  -- if not writes 0
+  aVal <- evalParameter a
+  bVal <- evalParameter b
+  locTgt <- evalAddress tgt
+  let res = if aVal == bVal then 1 else 0
+  setMemory locTgt res
+  moveNext
+runInstruction (Input tgt) = do
+  -- reads the next input from the input "stream"
+  -- and writes it to the address of the parameter
+  inp <- readInput
+  locTgt <- evalAddress tgt
+  setMemory locTgt inp
+  moveNext
+runInstruction (Output from) = do
+  -- gets the value from the parameter and writes it to the "output" stream
+  out <- evalParameter from
+  -- move next so the computer will continue after the output
+  moveNext
+  writeOutput out
+runInstruction (AdjustRelativeBase deltaP) = do
+  -- adjust the relative-base value
+  delta <- evalParameter deltaP
+  moveRelativeBase delta
+  moveNext
+
+
+-- | gets the instruction at the instruction-pointer
+getCurrentInstruction :: (Integral n, Show n) => IntCodeM n (Instruction n)
+getCurrentInstruction = do
+  adr <- getInstructionPointer
+  getInstruction adr
+
+
+-- | reads a instruction starting at the given memory address
+getInstruction :: forall n. (Integral n, Show n) => Address n -> IntCodeM n (Instruction n)
+getInstruction address = do
+  (opCode, modes) <- parseInstrCode <$> getMemory address
+  case opCode of
+    99 -> pure Halt
+    1  -> readInstruction' Add modes
+    2  -> readInstruction' Mult modes
+    3  -> readInstruction' Input modes
+    4  -> readInstruction' Output modes
+    5  -> readInstruction' JumpIfTrue modes
+    6  -> readInstruction' JumpIfFalse modes
+    7  -> readInstruction' LessThan modes
+    8  -> readInstruction' Equals modes
+    9  -> readInstruction' AdjustRelativeBase modes
+    _ -> Except.throwError $ Error $ "unknown opcode " ++ show opCode
+  where
+    readInstruction' i modes = (\(_,_,x) -> x) <$> readInstruction i (address+1) modes
+    parseInstrCode :: n -> (n,n)
+    parseInstrCode code =
+      let opCode = code `mod` 100
+          modes  = code `div` 100
+      in (opCode, modes)
+
+------------------------------------------------------------
+-- small helper class to help the compiler figure out the
+-- actual parameters-count while parsing for us
+
+class ReadInstruction n i | i -> n where
+  readInstruction :: i -> Address n -> n -> IntCodeM n (Address n, n, Instruction n)
+
+instance ReadInstruction n (Instruction n) where
+  readInstruction i adr modes = pure (adr, modes, i)
+
+instance (Integral n, Show n, Eq n, ReadInstruction n i) => ReadInstruction n (Parameter n -> i) where
+  readInstruction p2i adr modes = do
+    p <- readParam
+    readInstruction (p2i p) (adr+1) (modes `div` 10)
+    where
+      readParam = do
+        value <- getMemory adr
+        mode <- parameterModeFromInt (modes `mod` 10)
+        pure $ Parameter mode value
+
+------------------------------------------------------------
+
+-- | evaluates the parameter against the current memory content
+--   if it's an 'ImmediateMode' parameter it's just it's value
+--   if it's an 'PositionMode' parameter it's the memory content of the address
+--   = the parameters value
+evalParameter :: (Integral n, Show n) => Parameter n -> IntCodeM n n
+evalParameter (Parameter ImmediateMode v) = pure v
+evalParameter (Parameter PositionMode adr) = getMemory (fromIntegral adr)
+evalParameter (Parameter RelativeMode rel) = do
+  base <- getRelativeBase
+  getMemory (base + fromIntegral rel)
+
+-- | as of part 7 RelativeMode is allowed for output parameters too
+--   so this function takes care of adjusting the address to the relative
+--   base in this case
+evalAddress :: (Integral n, Show n) => Parameter n -> IntCodeM n (Address n)
+evalAddress (Parameter ImmediateMode _) =
+  Except.throwError $ Error "Immediate-Mode in Addressing"
+evalAddress (Parameter PositionMode adr) = pure adr
+evalAddress (Parameter RelativeMode rel) = do
+  base <- getRelativeBase
+  pure $ base + rel
+
+
+-- | reads the memory value at the given address
+getMemory :: (Ord n, Num n, Show n) => Address n -> IntCodeM n n
+getMemory adr | adr < 0 =
+  Except.throwError $ Error $ "tried to access the negative memory address " ++ show adr
+getMemory adr = do
+  found <- State.gets (Map.lookup adr . memory)
+  case found of
+    Just value -> pure value
+    Nothing    -> pure 0
+
+
+-- | sets the memory at the given address to a new value
+setMemory :: Ord n => MonadState (Computer n) m => Address n -> n -> m ()
+setMemory adr val =
+  State.modify (\prg -> prg { memory = Map.insert adr val (memory prg) })
+
+
+-- | get's the next user provided input if there are any
+--   if not interrupts the program requesting it
+readInput :: Show n => IntCodeM n n
+readInput = do
+  input <- State.gets nextInput
+  State.modify' (\s -> s { nextInput = Nothing })
+  case input of
+    Just n -> pure n
+    Nothing -> do
+      prg <- State.get
+      Except.throwError $ RequestInput (\inp -> Continuation ( prg { nextInput = Just inp }))
+
+
+-- | interrupts the program with an output
+writeOutput :: Show n => n -> IntCodeM n ()
+writeOutput out = do
+  prg <- State.get
+  Except.throwError $ NewOutput out (Continuation prg)
+
+
+-- | gets the current instruction pointer
+getInstructionPointer :: MonadState (Computer n) m => m (Address n)
+getInstructionPointer = State.gets instructionPointer
+
+
+-- | jumps to the given address by setting the instruction pointer to it
+jumpTo :: Address n -> IntCodeM n ()
+jumpTo = setInstructionPointer
+
+
+-- | moves the instruction-pointer to the next instruction after the current
+moveNext :: (Show n, Eq n, Integral n) => IntCodeM n ()
+moveNext = do
+  inst <- getCurrentInstruction
+  moveInstructionPointer (instructionLength inst)
+
+
+-- | moves the instruction-pointer to the current address increased by the given value
+moveInstructionPointer :: Num n => MonadState (Computer n) m => Address n -> m ()
+moveInstructionPointer delta = do
+  ip <- getInstructionPointer
+  setInstructionPointer (ip + delta)
+
+
+-- | sets the instruction pointer to the new value
+setInstructionPointer :: MonadState (Computer n) m => Address n -> m ()
+setInstructionPointer ip =
+  State.modify (\prg -> prg { instructionPointer = ip })
+
+
+-- | gets the current relative-base
+getRelativeBase :: MonadState (Computer n) m => m (Address n)
+getRelativeBase =
+  State.gets relativeBase
+
+
+-- | adjusts the current relative-base by adding the 'delta' paramter
+moveRelativeBase :: Num n => MonadState (Computer n) m => Address n -> m ()
+moveRelativeBase delta =
+  State.modify (\prg -> prg { relativeBase = relativeBase prg + delta })
+
+
+-- | parses a program from a string
+--   assumes the input are just integers separated by ',' characters and
+--   possible whitespace
+--   tries doing so by rewriting the input as a Haskell list and 'read' this
+parseProgram :: (Read n) => String -> Program n
+parseProgram input =
+  read $ "[" ++ input ++ "]"
