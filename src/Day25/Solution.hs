1634a3656f3c09ff0de5daf98cec20e7798836e4
Carsten König <Carsten.Koenig@hotmail.de>
2019-12-19 16:10:16 +0100

    prepared remaining days

---
+++ src/Day25/Solution.hs
@@ -0,0 +1,36 @@
+module Day25.Solution where
+
+import           CommonParsers
+import           ConsoleTests
+
+
+dayNr :: Int
+dayNr = 25
+
+type Input = String
+
+run :: IO ()
+run = do
+  putStrLn $ "DAY " ++ show dayNr
+
+  inp <- loadInput
+
+  let res1 = part1 inp
+  putStrLn $ "\t Part 1: " ++ show res1
+
+  let res2 = part2 inp
+  putStrLn $ "\t Part 2: " ++ show res2
+
+  putStrLn "---\n"
+
+
+part1 :: Input -> Int
+part1 inp = undefined
+
+
+part2 :: Input -> Int
+part2 inp = undefined
+
+
+loadInput :: IO Input
+loadInput = readFile $ "./src/Day" ++ show dayNr ++ "/input.txt"
